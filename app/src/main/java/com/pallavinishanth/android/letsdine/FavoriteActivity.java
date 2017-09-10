5ecd171c13a389420d5bc143a2c4db8be85cebd7
PallaviNishanth <pallavinishanth@gmail.com>
2017-09-10 19:34:00 -0400

    feat: Content provider added

--- app/src/main/java/com/pallavinishanth/android/letsdine/FavoriteActivity.java
+++ app/src/main/java/com/pallavinishanth/android/letsdine/FavoriteActivity.java
@@ -49,7 +49,7 @@ public class FavoriteActivity extends AppCompatActivity {
             @Override
             public void onItemClick(View itemView, int position) {
 
-                Toast.makeText(FavoriteActivity.this, "Fav Restaurant clicked", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(FavoriteActivity.this, "Fav Restaurant clicked", Toast.LENGTH_SHORT).show();
 
                 Intent i = new Intent(FavoriteActivity.this, DetailActivity.class);
                 i.putExtra(DetailActivity.PLACE_ID, PlaceID[position]);
