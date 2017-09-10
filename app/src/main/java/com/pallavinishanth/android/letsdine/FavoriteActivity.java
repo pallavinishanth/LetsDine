b53b2cadfffd121e960c46dcffa5e14e2ff0bc37
PallaviNishanth <pallavinishanth@gmail.com>
2017-09-10 00:01:16 -0400

    feat: Content provider added

---
+++ app/src/main/java/com/pallavinishanth/android/letsdine/FavoriteActivity.java
@@ -0,0 +1,100 @@
+package com.pallavinishanth.android.letsdine;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.pallavinishanth.android.letsdine.Data.ResContract;
+
+/**
+ * Created by PallaviNishanth on 9/8/17.
+ */
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = FavoriteActivity.class.getSimpleName();
+
+    private FavoriteAdapter favAdapter;
+    private RecyclerView favRecyclerView;
+    private RecyclerView.LayoutManager favLayoutManager;
+
+    String[] res_names;
+    String[] res_address;
+    String PlaceID;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.favorite_layout);
+
+        favRecyclerView = (RecyclerView) findViewById(R.id.fav_recycler_view);
+        favRecyclerView.setHasFixedSize(true);
+        favLayoutManager = new LinearLayoutManager(FavoriteActivity.this,
+                LinearLayoutManager.VERTICAL, false);
+        favRecyclerView.setLayoutManager(favLayoutManager);
+
+        favAdapter = new FavoriteAdapter(getBaseContext(), res_names, res_address);
+        favRecyclerView.setAdapter(favAdapter);
+
+        favAdapter.setOnItemClickListener(new FavoriteAdapter.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(View itemView, int position) {
+
+                Toast.makeText(FavoriteActivity.this, "Fav Restaurant clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getFavRestaurants();
+    }
+
+    public void getFavRestaurants() {
+
+        Log.v(LOG_TAG, "Get favorite Restaurants");
+
+        Cursor favrescursor;
+
+        favrescursor = getContentResolver().
+                query(ResContract.ResEntry.CONTENT_URI, null, null, null, null, null);
+
+        if(favrescursor.getCount()!=0) {
+
+            Log.v(LOG_TAG, "Fav movies DB has items" + favrescursor.getCount());
+
+            favrescursor.moveToFirst();
+            int i = 0;
+
+            res_names = new String[favrescursor.getCount()];
+            res_address = new String[favrescursor.getCount()];
+
+            do{
+
+                res_names[i] = favrescursor.
+                        getString(favrescursor.getColumnIndex(ResContract.ResEntry.COLUMN_RES_NAME));
+                res_address[i] = favrescursor.
+                        getString(favrescursor.getColumnIndex(ResContract.ResEntry.COLUMN_RES_VICINITY));
+                i++;
+
+            }while(favrescursor.moveToNext());
+            favrescursor.close();
+
+            favAdapter = new FavoriteAdapter(getBaseContext(), res_names, res_address);
+            favRecyclerView.setAdapter(favAdapter);
+        } else{
+
+            Toast.makeText(FavoriteActivity.this, "No Restaurants in Favorite List", Toast.LENGTH_SHORT).show();
+            favrescursor.close();
+        }
+    }
+}
